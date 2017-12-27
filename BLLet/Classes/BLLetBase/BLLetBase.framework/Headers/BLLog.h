8271e091eeee6d9757102943e83e332f35e61b15
zhujunjie <zhujunjie@zhujunjiedeiMac.local>
2017-12-27 17:16:33 +0800

    修改编译脚本

--- BLLet/Classes/BLLetBase/BLLetBase.framework/Headers/BLLog.h
+++ BLLet/Classes/BLLetBase/BLLetBase.framework/Headers/BLLog.h
@@ -9,9 +9,11 @@
 #ifndef BLLog_h
 #define BLLog_h
 
-#define BLLogError(frmt, ...)    do{ NSLog((@"[BLLet ERROR]"frmt), ##__VA_ARGS__); } while(0)
-#define BLLogWarn(frmt, ...)     do{ NSLog((@"[BLLet WARN]"frmt), ##__VA_ARGS__); } while(0)
-#define BLLogDebug(frmt, ...)    do{ NSLog((@"[BLLet DEBUG]"frmt), ##__VA_ARGS__); } while(0)
-#define BLLogVerbose(frmt, ...)  do{ NSLog((@"[BLLet VERBOSE]"frmt), ##__VA_ARGS__); } while(0)
+#import "BLLogLevel.h"
+
+#define BLLogError(frmt, ...)    do{ if([BLLogLevel sharedLevel].level >= 1) NSLog((@"[BLLet ERROR]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogWarn(frmt, ...)     do{ if([BLLogLevel sharedLevel].level >= 2) NSLog((@"[BLLet WARN]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogDebug(frmt, ...)    do{ if([BLLogLevel sharedLevel].level >= 3)  NSLog((@"[BLLet DEBUG]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogVerbose(frmt, ...)  do{ if([BLLogLevel sharedLevel].level >= 4)  NSLog((@"[BLLet VERBOSE]"frmt), ##__VA_ARGS__); } while(0)
 
 #endif /* BLLog_h */
