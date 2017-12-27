7ca58558e5ea1cfcdeb0fa41f0fdd6688b95ec41
zhujunjie <zhujunjie@zhujunjiedeiMac.local>
2017-12-27 16:55:33 +0800

    修改重复代码

--- BLLet/Classes/BLLetBase/BLLetBase.framework/Headers/BLLog.h
+++ BLLet/Classes/BLLetBase/BLLetBase.framework/Headers/BLLog.h
@@ -9,13 +9,9 @@
 #ifndef BLLog_h
 #define BLLog_h
 
-int BL_NSLOG_LEVEL = 0;
-
-extern int BL_NSLOG_LEVEL;
-
-#define BLLogError(frmt, ...)    do{ if(BL_NSLOG_LEVEL >= 1) NSLog((@"[BLLet ERROR]"frmt), ##__VA_ARGS__); } while(0)
-#define BLLogWarn(frmt, ...)     do{ if(BL_NSLOG_LEVEL >= 2) NSLog((@"[BLLet WARN]"frmt), ##__VA_ARGS__); } while(0)
-#define BLLogDebug(frmt, ...)    do{ if(BL_NSLOG_LEVEL >= 3) NSLog((@"[BLLet DEBUG]"frmt), ##__VA_ARGS__); } while(0)
-#define BLLogVerbose(frmt, ...)  do{ if(BL_NSLOG_LEVEL >= 4) NSLog((@"[BLLet VERBOSE]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogError(frmt, ...)    do{ NSLog((@"[BLLet ERROR]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogWarn(frmt, ...)     do{ NSLog((@"[BLLet WARN]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogDebug(frmt, ...)    do{ NSLog((@"[BLLet DEBUG]"frmt), ##__VA_ARGS__); } while(0)
+#define BLLogVerbose(frmt, ...)  do{ NSLog((@"[BLLet VERBOSE]"frmt), ##__VA_ARGS__); } while(0)
 
 #endif /* BLLog_h */
