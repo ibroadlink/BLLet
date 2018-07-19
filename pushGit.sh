f4271bfe440037e1486719fb24b857187aef1b14
hongkun.bai <hongkun@zhujunjiedeiMac.local>
2018-07-19 17:24:45 +0800

    更新dnasdk

--- pushGit.sh
+++ pushGit.sh
@@ -5,18 +5,12 @@ if [[ $# != 1 ]]; then
 	exit 1;
 fi
 
-cd ..
 svnlog=`svn log -l 1 | grep Reviewed-by | cut -d " " -f 1`
 gittag=$1
 
 echo $svnlog
 echo $gittag
 
-cp -rf BLLetBase/build/Release-iphonelipo/BLLetBase.framework BLLet/BLLet/Classes/BLLetBase/
-cp -rf BLLetCore/build/Release-iphonelipo/BLLetCore.framework BLLet/BLLet/Classes/BLLetCore/
-cp -rf BLLetPlugins/build/Release-iphonelipo/BLLetPlugins.framework BLLet/BLLet/Classes/BLLetPlugins/
-cp -rf BLLetCloud/build/Release-iphonelipo/BLLetCloud.framework BLLet/BLLet/Classes/BLLetCloud/
-cd BLLet
 
 git commit -a -m $svnlog
 git push 
